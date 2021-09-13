5509423fd2907a56dabe6fa7189dde4b5614117a
sehbas04 <sehbas82@gmail.com>
2021-09-13 17:29:05 +0530

    video 5 and 6 examples

---
+++ src/second/MultiValueSelectionExample.java
@@ -0,0 +1,24 @@
+package second;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class MultiValueSelectionExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver","D:\\Automation\\Driver\\chromedriver.exe");
+		WebDriver driver= new ChromeDriver();
+		
+		driver.get("https://semantic-ui.com/modules/dropdown.html");
+		//initialize the select class
+		Select obj= new Select(driver.findElement(By.className("//*[@class='ui fluid dropdown selection multiple']")));
+	
+		//select and deselect command
+		 obj.selectByVisibleText("");
+		
+		}
+
+}
